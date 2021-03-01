164643c07c0ae92029e4c17071fa9811934c90c6
Dedas <andreas.winkler@winkit.se>
2021-03-01 15:19:05 +0100

    added intent

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import os
+import random
 
 import discord
 from environs import load_dotenv
@@ -7,10 +8,27 @@ from environs import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    message_content = message.content.lower()
+    if "singla" in message_content:
+        rand_int = random.randint(0, 1)
+        if rand_int == 0:
+            results = "Krona"
+        else:
+            results = "Klave"
+        await message.channel.send(results)
 
 client.run(TOKEN)
