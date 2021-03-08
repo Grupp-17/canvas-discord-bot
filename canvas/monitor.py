66b152fbd1dc770395b2c6f41c4b38e8cbaab20d
Dedas <andreas.winkler@winkit.se>
2021-03-09 00:29:52 +0100

    added scheduler

---
+++ canvas/monitor.py
@@ -0,0 +1,41 @@
+# Internal modules
+from datetime import datetime
+
+# Local modules
+from canvas.http_requests import fetch_courses
+from database.interactions import sql_query_commit
+from database.queries import sql_insert_into
+
+# Third party modules
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def init_monitor():
+    # Create scheduler object
+    scheduler = BackgroundScheduler()
+
+    # Set scheduler action and interval
+    scheduler.add_job(update_courselist, 'interval', seconds=5)
+
+    # Run scheduler
+    scheduler.start()
+
+    
+def update_courselist():
+
+    # Fetch coursedata
+    data = fetch_courses()
+
+    if data is not None:
+
+        # Insert every fetched course
+        for i in range(len(data)):
+            sql_query_commit(sql_insert_into(data[i]['name'], data[i]['course_code'], data[i]['created_at']))
+        print('Courses inserted: ' + str(datetime.now()))
+    else:
+        print('Database error!')
+
+
+# Test
+#with open('test.json', 'r') as jsonfile:
+#    data = json.load(jsonfile)
+#    print(data[0]['id'])
