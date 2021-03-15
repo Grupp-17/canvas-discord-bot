02419cb17e6e4f29bcd7707d8470989bfe14e8dd
Tove-A <toveandersson13@gmail.com>
2021-03-15 20:55:55 +0100

    embed fixes

--- discord_api/commands/announcement.py
+++ discord_api/commands/announcement.py
@@ -11,23 +11,32 @@ def announcement(announcement_id):
     course_id = context_code.strip("course_")
     
     if is_subscribed(course_id):
-        return create_announcements_embed(announcement_id)
+        return create_announcements_embed(announcement_id, course_id)
     else:
         return None
 
-def create_announcements_embed(announcement_id):
+def create_announcements_embed(announcement_id, course_id):
 
     announcement = create_sql_query_list(sql_query_fetch(sql_select_table_attributes_condition("*", "announcements", f"id = '{announcement_id}'")))
+    course = create_sql_query_list(sql_query_fetch(sql_select_table_attributes_condition("*", "courses", f"id = '{course_id}'")))
 
     id = announcement[0]
     title = announcement[1]
     message = announcement[2]
     author = announcement[3]
     context_code = announcement[4]
-    posted_at = announcement[5]
+    posted_at = announcement[5].replace("T", " ")
+    posted_at_formatted = posted_at.replace("Z", "")
+
+
+    course_name = course[1]
     
-    embed = discord.Embed(title="Anslag", 
-                          description=posted_at, 
+    embed = discord.Embed(title="Announcement", 
+                          description=f"Posted on {course_name} \n"+ posted_at_formatted, 
                           colour=0x98FB98)
+
+    embed.set_author(name="CanvasDiscordBot", 
+                    icon_url="https://play-lh.googleusercontent.com/2_M-EEPXb2xTMQSTZpSUefHR3TjgOCsawM3pjVG47jI-BrHoXGhKBpdEHeLElT95060B=s180")
+
     embed.add_field(name=title, value=message, inline=False)
     return embed
