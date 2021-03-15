7a53396a502bb70861962afee22790440547f655
Dedas <andreas.winkler@winkit.se>
2021-03-15 20:23:38 +0100

    removed rest of singla

--- discord_api/commands/command_base.py
+++ discord_api/commands/command_base.py
@@ -1,19 +1,14 @@
 # Registrering commands to Discord bot
 
-# Internal modules
-import random
-
 # Local modules
 from discord.ext.commands.core import command
 from database.interactions import *
 from database.queries import *
 from discord_api.commands.courses import courses_command
-from discord_api.commands.krona_klave import krona_klave_command
 from discord_api.commands.subscribe import subscribe_command
 
 # Third party modules
 from discord.ext import commands
-
 
 class CommandBase(commands.Cog):
     def __init__(self, client):
