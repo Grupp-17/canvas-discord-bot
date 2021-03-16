01189cba705819b3a3050d74175e66293aee4542
Tove-A <toveandersson13@gmail.com>
2021-03-16 11:01:36 +0100

    All commands and command_base commented

--- discord_api/commands/courses.py
+++ discord_api/commands/courses.py
@@ -1,35 +1,29 @@
+# Courses command
+
+# Local modules
 from database.interactions import *
 from database.queries import *
+
+# Third party modules
 import discord
 
-# TODO Comments
-
 def courses_command(ctx):
-    course_names_query = sql_query_fetch(sql_select_table_attributes("name", "courses"))
-    course_codes_query = sql_query_fetch(sql_select_table_attributes("course_code", "courses"))
-    course_subscribed_query = sql_query_fetch(sql_select_table_attributes("subscribed_to", "courses"))
-    id_query = sql_query_fetch(sql_select_table_attributes("id", "courses"))
-    
-    course_names = []
-    course_codes = []
-    course_subscribed = []
-    id_list = []
 
-    for names, codes, sub, id in zip(course_names_query, course_codes_query, course_subscribed_query, id_query):
-        course_names.append(names[0])
-        course_codes.append(codes[0])
-        course_subscribed.append(sub[0])
-        id_list.append(id[0])
+    # Queries to get lists containing information about all courses
+    course_names = create_sql_query_list(sql_query_fetch(sql_select_table_attributes("name", "courses")))
+    course_codes = create_sql_query_list(sql_query_fetch(sql_select_table_attributes("course_code", "courses")))
+    course_subscribed_to = create_sql_query_list(sql_query_fetch(sql_select_table_attributes("subscribed_to", "courses")))
+    id_list = create_sql_query_list(sql_query_fetch(sql_select_table_attributes("id", "courses")))
 
-    # Embed for displaying courses in discord
+    # Embed for displaying all availible courses in discord
     embed = discord.Embed(title='Avaliable courses', 
                         colour=0x98FB98)
     
     embed.set_author(name="CanvasDiscordBot", 
                     icon_url="https://play-lh.googleusercontent.com/2_M-EEPXb2xTMQSTZpSUefHR3TjgOCsawM3pjVG47jI-BrHoXGhKBpdEHeLElT95060B=s180")
     
-    # Add fields in the embed by iterating through the lists
-    for name, code, sub, id in zip(course_names, course_codes, course_subscribed, id_list):
+    # Adding the fields in the embed by iterating through the lists
+    for name, code, sub, id in zip(course_names, course_codes, course_subscribed_to, id_list):
         if sub == 1:
             sub = "🔔"
         else:
--- discord_api/commands/subscribe.py
+++ discord_api/commands/subscribe.py
@@ -1,10 +1,16 @@
+
+# Local modules
 from database.interactions import *
 from database.queries import *
+
+# Third party modules
 import discord
 
-# TODO Comments
 
+# Checks if a course is subscribed to
 def is_subscribed(course_id):
+
+    # Query to get subscribed_to value from courses
     subscribed = sql_query_fetchone_result(sql_select_table_attributes_condition("subscribed_to", "courses", f"id = {course_id}"))
 
     if subscribed == 1:
@@ -12,35 +18,54 @@ def is_subscribed(course_id):
     else:
         return False
 
+# Subscribe command
 def subscribe_command(arg):
 
+    # Query to get id, name, course_code from a specific course
     course_info = create_sql_query_list(sql_query_fetch(sql_select_table_attributes_condition("id, name, course_code", "courses", f"id == '{arg}'")))
+    
+    # Query to get id for all courses
     id_query = create_sql_query_list(sql_query_fetch(sql_select_table_attributes("id", "courses")))
 
+    # Check if course_info contains information
     if (course_info != []):
+
+        # Declare the information into variables
         course_id = course_info[0]
         course_name = course_info[1]
         course_code = course_info[2]
+
+        # Embed layout
         embed = discord.Embed(colour=0x98FB98, description="🔔✔")
         embed.set_author(name="CanvasDiscordBot",
                         icon_url="https://play-lh.googleusercontent.com/2_M-EEPXb2xTMQSTZpSUefHR3TjgOCsawM3pjVG47jI-BrHoXGhKBpdEHeLElT95060B=s180")
+        
+        # Check if course is subscribed to
         if is_subscribed(arg):
+
             embed.add_field(name=f"\n\nAlready subscribed to course {course_id}", value=f"{course_name} | {course_code}\n")
             embed.set_footer(text="No subscription added!")
+
         else:
+
             embed.add_field(name=f"\n\nSubscribed to course {arg}", value=f"{course_name} | {course_code}\n")
             embed.set_footer(text="Subscription added ✔️")
+
+            # Update subscribed_to on the course that now is subscribed to
             sql_query_commit(sql_update_subscription(arg, "1"))
-            
-            # TODO Remove checks or create suitable debugging message
-            #check = sql_query_fetchone_result(sql_select_table_attributes_condition("subscribed_to", "courses", f"id = {arg}"))
-            #print(check)
+
         return embed
+
     else:
+
+        # If the course does not exist in the database,
+        # send embed with that information and the availible courses
+
         all_courses_id = "\n".join(str(i) for i in id_query)
         embed = discord.Embed(colour=0x98FB98, description="🤷")
         embed.set_author(name="CanvasDiscordBot",
                         icon_url="https://play-lh.googleusercontent.com/2_M-EEPXb2xTMQSTZpSUefHR3TjgOCsawM3pjVG47jI-BrHoXGhKBpdEHeLElT95060B=s180")
         embed.add_field(name=f"\n\nCourse '{arg}' does not exist", value=f"Avalible Courses:\n{all_courses_id}")
         embed.set_footer(text="No subscription added!")
+
         return embed
