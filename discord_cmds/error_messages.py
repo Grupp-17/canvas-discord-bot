f0e077067aa22b1a6100ed324d4b6f981737ee7a
Tove-A <toveandersson13@gmail.com>
2021-04-08 16:19:44 +0200

    changed variable

--- discord_cmds/error_messages.py
+++ discord_cmds/error_messages.py
@@ -1,6 +1,6 @@
 import discord
 
-def error_message_embed(title, error_type, message, fix):
+def error_message_embed(title, error_type, message, user_action):
 
     embed = discord.Embed(title=f'{title} ⚠️', 
                           description=f'{error_type}', 
@@ -9,6 +9,6 @@ def error_message_embed(title, error_type, message, fix):
     embed.set_author(name='CanvasDiscordBot', 
                     icon_url='https://play-lh.googleusercontent.com/2_M-EEPXb2xTMQSTZpSUefHR3TjgOCsawM3pjVG47jI-BrHoXGhKBpdEHeLElT95060B=s180')
 
-    embed.add_field(name=f'{message}', value=f'{fix}' + '\n\n\n ', inline=False)
+    embed.add_field(name=f'{message}', value=f'{user_action}' + '\n\n\n ', inline=False)
 
     return embed
