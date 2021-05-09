acd846f2c64335e0cb027474f44f6f3f754db092
Dedas <andreas.winkler@winkit.se>
2021-05-09 16:29:16 +0200

    configured log handler

--- log_handler.py
+++ log_handler.py
@@ -1,5 +1,6 @@
 # Local modules
 import logging
+from logging.handlers import RotatingFileHandler
 
 # Internal modules
 from utils import \
@@ -10,19 +11,21 @@ from utils import \
 logger = logging.getLogger()
 
 # How the log data is formatted
-log_format = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s')
+log_format = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] [%(module)s] [%(lineno)d] %(message)s')
+
+RotatingFileHandler('logs/debug.log', maxBytes=2000, backupCount=10)
 
 if(get_config('debug_log') or get_debug()):
     logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler('debug.log')
+    file_handler = RotatingFileHandler('logs/debug.log', maxBytes=2000000, backupCount=10)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
 
-    print('DEBUG MODE -> debug.log')
+    print('DEBUG MODE | Output: logs/debug.log')
 else:    
     logger.setLevel(logging.WARNING)
-    file_handler = logging.FileHandler('error.log')
+    file_handler = RotatingFileHandler('logs/error.log', maxBytes=2000000, backupCount=10)
     file_handler.setLevel(logging.WARNING)
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
